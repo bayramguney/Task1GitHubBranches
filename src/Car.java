48aff900dbdf44a52ae8b9bd6b944dbf22267e11
bayramguney <bayramguney@yahoo.com>
2019-11-18 16:24:57 -0500

    added Carclass

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    public String name;
+    public int speed;
+    public String engine;
+    public int doors;
+    public String color;
+
+    public void drive(){
+
+    }
+    public void isElectric(){
+
+    }
+    public void isFast(){
+
+    }
+}
